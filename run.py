c53e9f63d61fb714f7aee38bc52efdbd2f12e5a9
CyperStone <szymoncyperski@gmail.com>
2023-09-04 11:51:05 +0200

    change usage of script's arguments

--- run.py
+++ run.py
@@ -32,9 +32,9 @@ if __name__ == '__main__':
     )
     required.add_argument('-i', '--image_filename', required=True, type=str,
                           help=f'input image filename (must be placed in {INPUT_DIR}) directory')
-    optional.add_argument('--n_iter', default=DEFAULT_RUN_PARAMS['n_iter'], type=float,
+    optional.add_argument('--n_iter', default=None, type=float,
                           help='number of algorithm iterations (generations)')
-    optional.add_argument('--n_seconds', default=DEFAULT_RUN_PARAMS['n_seconds'], type=float,
+    optional.add_argument('--n_seconds', default=None, type=float,
                           help='duration of algorithm execution (in seconds)')
     optional.add_argument('--no_animation', action='store_false', dest='animation',
                           help='do not save an evolution progress as a GIF animation')
@@ -42,6 +42,17 @@ if __name__ == '__main__':
                           help='frequency at which algorithm outputs are included in the GIF animation'
                                '(e.g. 10 for every 10th iteration)')
     args = parser.parse_args()
+
+    n_iter = args.n_iter
+    n_seconds = args.n_seconds
+    animation = args.animation
+    img_history_step = args.img_history_step
+
+    if args.n_iter is None and args.n_seconds is None:
+        n_iter = DEFAULT_RUN_PARAMS['n_iter']
+
+    if not animation:
+        img_history_step = None
 
     input_img = Image.open(os.path.join(INPUT_DIR, args.image_filename))
     input_img_array = np.array(input_img)
@@ -51,16 +62,16 @@ if __name__ == '__main__':
 
     best_img, _, current_best_img_history, _, _, _ = ga.fit(
         task=task,
-        n_iter=args.n_iter,
-        n_seconds=args.n_seconds,
-        img_history_step=args.img_history_step
+        n_iter=n_iter,
+        n_seconds=n_seconds,
+        img_history_step=img_history_step
     )
 
     current_time = datetime.today().strftime('%Y-%m-%d_%H-%M')
     output_filename = f'{args.image_filename.split(".")[0]}_{OUTPUT_FILENAME_SUFFIX}_{current_time}.jpg'
     Image.fromarray(best_img).save(os.path.join(OUTPUT_DIR, output_filename))
 
-    if args.animation:
+    if animation:
         input_img_array_resized = resize_image(input_img_array, width=ANIMATION_IMAGE_WIDTH)
 
         animation_filename = f'{args.image_filename.split(".")[0]}_{ANIMATION_FILENAME_SUFFIX}_{current_time}.gif'
